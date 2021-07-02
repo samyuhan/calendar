5c0920d086c7d02da2f556f6a49065b9053ea39a
Samantha Yuhan <sysweettooth1@gmail.com>
2021-07-01 22:11:40 -0700

    create function to coordinate colors based on past, present, future

--- script.js
+++ script.js
@@ -2,5 +2,29 @@
 var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").append(currentDay);
 
+// Create currentTime variable to coordinate colors for past, present, future
+var currentTime = moment();
+colorBlock();
 
+// Coordinate colors with this function
+function colorBlock(){
+    var timeBlock = $(".time-block");
+    var currentHr = currentTime.hours();
+    
+    for(var i = 0; i < timeBlock.length; i++){
+        var block = timeBlock[i];
 
+        if(parseInt(block.id) < currentHr){
+            $(block).addClass("past");
+        }
+        else if(parseInt(block.id) === currentHr){
+            $(block).removeClass("past");
+            $(block).addClass("present");
+        }
+        else{
+            $(block).removeClass("past");
+            $(block).removeClass("present");
+            $(block).addClass("future");
+        }
+    }
+}
